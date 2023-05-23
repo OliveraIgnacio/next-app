55e78f6fbb95c73a2e6a621fea1f15df1344d05f
Nacho Olivera <oliveraignaciook@gmail.com>
2023-05-22 21:30:01 -0300

    pagina basica

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import PageLayout from "../components/PageLayout"
+
+export default function about() {
+    return <PageLayout title='About'>
+        <h1>Esta es la pagina del about</h1>
+    </PageLayout>
+}
